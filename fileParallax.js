0e84940e8d6ef504f60c9e07adf702ae71e5d00e
medonosec <medonosec@gmail.com>
2015-08-20 10:51:11 +0300

    Created file

---
+++ fileParallax.js
@@ -0,0 +1,25 @@
+$(".timer-block").parallaxMd();
+$(".block-top-1").parallaxMd({
+    "imgHeight": "80%",
+    "imgWidth": "100%",
+    "speed": "15",
+    "container": 2
+});
+$(".large-image-1").parallaxMd({
+  "imgHeight": "550px",
+  "imgWidth": "100%",
+  "speed": "15",
+  "container": 3
+});
+$(".block-photo-par").parallaxMd({
+  "imgHeight": "400px",
+  "imgWidth": "100%",
+  "speed": "15",
+  "container": 4
+});
+$(".simple-block").parallaxMd({
+  "imgHeight": "400px",
+  "imgWidth": "100%",
+  "speed": "12",
+  "container": 5
+});
